563adc5133913cb6856bb7f8bddf8b92312938aa
shaunryan0701 <shaunryan0701@googlemail.com>
2024-05-03 18:46:27 +0100

    Working version that connects to dydx testnet

--- program/main.py
+++ program/main.py
@@ -1,3 +1,10 @@
+from func_connections import connect_to_dydx
 
 if __name__ == "__main__":
-    print("Hello world!")
+    # Connect to client
+    try:
+        client = connect_to_dydx()
+    except Exception as e:
+        print(e)
+        print("Error connecting to client", e)
+        exit(1)
